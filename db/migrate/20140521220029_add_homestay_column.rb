4de91215a614f9a53d05e9a4f104acc8480abd31
Kristen Tonga <kristen.tonga@gmail.com>
2014-05-21 22:17:31 -0400

    deleted migrations to simplify

--- db/migrate/20140521220029_add_homestay_column.rb
+++
@@ -1,8 +0,0 @@
-class AddHomestayColumn < ActiveRecord::Migration
-  def change
-    add_column :schools, :homestay, :text
-    add_column :schools, :homestay_price, :integer
-    add_column :schools, :meals_incl, :boolean
-    add_column :schools, :meals_add_price, :integer
-  end
-end
